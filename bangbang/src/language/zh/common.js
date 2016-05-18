c03d370369373fc854a4b6fc46e235f297921d77
marlowe <478592396@qq.com>
2016-05-18 11:26:04 +0800

    grunt  构建项目

---
+++ bangbang/src/language/zh/common.js
@@ -0,0 +1,14 @@
+/**
+ *  语言包
+ * @module bfw
+ * @author blank
+ * @date   20151118
+ * @version 1.0.0
+ */
+
+module.exports = {
+        'system_title' : 'tiger-fw',
+		'404'     : 'Not Found!!',
+		'aass'    : 'kakakakaka',
+		'testone' : 'ajflkasjdl {aa} ,mfgdsalgjla {aa} lkgjdlsajga{bb}'
+};
